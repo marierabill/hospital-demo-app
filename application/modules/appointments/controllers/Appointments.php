1f254c80ce7df0458dca10366aa49c4811a86034
Bill Mariera <71690762+marierabill@users.noreply.github.com>
2025-09-28 11:48:34 +0300

    HMVC structure1

---
+++ application/modules/appointments/controllers/Appointments.php
@@ -0,0 +1,113 @@
+<?php
+class Appointments extends MX_Controller 
+{
+
+function __construct() {
+parent::__construct();
+}
+
+
+public function schedule() {
+    if ($this->input->post()) {
+        $data = [
+            'patient_id' => $this->input->post('patient_id'),
+            'doctor'     => $this->input->post('doctor'),
+            'date'       => $this->input->post('date'),
+            'time'       => $this->input->post('time')
+        ];
+        $this->Appointment_model->insert($data);
+        redirect('appointments');
+    }
+    $data['patients'] = $this->Patient_model->get_all();
+    $this->load->view('appointments/schedule', $data);
+}
+
+
+
+
+
+
+function get($order_by)
+{
+    $this->load->model('mdl_perfectcontroller');
+    $query = $this->mdl_perfectcontroller->get($order_by);
+    return $query;
+}
+
+function get_with_limit($limit, $offset, $order_by) 
+{
+    if ((!is_numeric($limit)) || (!is_numeric($offset))) {
+        die('Non-numeric variable!');
+    }
+
+    $this->load->model('mdl_perfectcontroller');
+    $query = $this->mdl_perfectcontroller->get_with_limit($limit, $offset, $order_by);
+    return $query;
+}
+
+function get_where($id)
+{
+    if (!is_numeric($id)) {
+        die('Non-numeric variable!');
+    }
+
+    $this->load->model('mdl_perfectcontroller');
+    $query = $this->mdl_perfectcontroller->get_where($id);
+    return $query;
+}
+
+function get_where_custom($col, $value) 
+{
+    $this->load->model('mdl_perfectcontroller');
+    $query = $this->mdl_perfectcontroller->get_where_custom($col, $value);
+    return $query;
+}
+
+function _insert($data)
+{
+    $this->load->model('mdl_perfectcontroller');
+    $this->mdl_perfectcontroller->_insert($data);
+}
+
+function _update($id, $data)
+{
+    if (!is_numeric($id)) {
+        die('Non-numeric variable!');
+    }
+
+    $this->load->model('mdl_perfectcontroller');
+    $this->mdl_perfectcontroller->_update($id, $data);
+}
+
+function _delete($id)
+{
+    if (!is_numeric($id)) {
+        die('Non-numeric variable!');
+    }
+
+    $this->load->model('mdl_perfectcontroller');
+    $this->mdl_perfectcontroller->_delete($id);
+}
+
+function count_where($column, $value) 
+{
+    $this->load->model('mdl_perfectcontroller');
+    $count = $this->mdl_perfectcontroller->count_where($column, $value);
+    return $count;
+}
+
+function get_max() 
+{
+    $this->load->model('mdl_perfectcontroller');
+    $max_id = $this->mdl_perfectcontroller->get_max();
+    return $max_id;
+}
+
+function _custom_query($mysql_query) 
+{
+    $this->load->model('mdl_perfectcontroller');
+    $query = $this->mdl_perfectcontroller->_custom_query($mysql_query);
+    return $query;
+}
+
+}
